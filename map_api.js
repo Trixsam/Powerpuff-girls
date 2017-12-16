6302284468a8734b70b75a29a6cc8b6377cf3747
Trixsam <samantha_pikero@yahoo.com>
2017-12-16 22:31:28 +0100

    Inserted the Map API into a seperate .js folder

---
+++ map_api.js
@@ -0,0 +1,14 @@
+function initMap() {
+    var myLatLng = {lat: 52.370, lng: 4.895};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 13,
+      center: myLatLng
+    });
+
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      title: 'Hello World!'
+    });
+  };
